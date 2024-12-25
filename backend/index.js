b39f9cc54a311137056dec4734c4e3c91108d544
Praveen <praveenpatidar171@gmail.com>
2024-12-25 16:47:33 +0530

    Build added

--- backend/index.js
+++ backend/index.js
@@ -7,17 +7,26 @@ const userRoutes = require('./routes/userRoutes');
 const accountRoutes = require('./routes/accountRoutes');
 const cors = require('cors');
 
+const path = require('path');
+
 connectdb();
 const PORT = process.env.PORT || 5000;
-
-
 const app = express();
+
+const _dirname = path.resolve();
+
 app.use(cors());
 app.use(express.json());
 
 
 app.use('/api/v1/user', userRoutes);
 app.use('/api/v1/account', accountRoutes);
+
+
+app.use(express.static(path.join(_dirname, "/frontend/dist")));
+app.get('*', (req, res) => {
+ res.sendFile(path.resolve(_dirname, "frontend", "dist" , "index.html"))
+})
 
 app.listen(PORT, () => {
     console.log(`Example app listening on port ${PORT}`);
